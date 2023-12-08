6a7648b0a721e84fcffef0ba098ea2fd0e152bbc
John <johnpua@outlook.com>
2023-12-08 06:44:33 -0800

    Bcrypt

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,3 +1,5 @@
 from flask import render_template,session,request,redirect
 from flask_app import app
 from flask_app.models.user import User
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
